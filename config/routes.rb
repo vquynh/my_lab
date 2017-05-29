507611bf938da64e424f79136d93b51ba917cc9e
nvquynh <quynh.nguyen@haw-hamburg.de>
2017-05-29 18:03:30 +0200

    added home about and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'about', to: 'page#about'
+
+  get 'contact', to: 'page#contact'
+
+  root to: 'page#home'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
