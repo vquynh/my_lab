9e955d08f8dc6d10e98cec452256fa70d659a2aa
nvquynh <quynh.nguyen@haw-hamburg.de>
2017-06-18 14:24:21 +0200

    added session tracker

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -6,10 +6,11 @@ module CurrentUserConcern
     end
 
     def guest_user
-        OpenStruct.new(name: "Guest User", 
-                    first_name: "Guest", 
-                    last_name: "User", 
-                    email: "guest@example.com")
+        OpenStruct.new(name: "Guest User",
+                       first_name: "Guest",
+                       last_name: "User",
+                       email: "guest@example.com"
+                       )
     end
 
 end
