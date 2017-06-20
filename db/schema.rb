8ec9e86b7c72118878ac02fdc7bbabfc9cd5480c
vquynh <ng.viet.quynh@gmail.com>
2017-06-20 13:04:49 +0000

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620113809) do
+ActiveRecord::Schema.define(version: 20170620124939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,20 @@ ActiveRecord::Schema.define(version: 20170620113809) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "project_id"
+    t.bigint "booking_status_id"
+    t.date "pickup_date"
+    t.date "return_date"
+    t.uuid "uuid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_status_id"], name: "index_bookings_on_booking_status_id"
+    t.index ["project_id"], name: "index_bookings_on_project_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -76,6 +90,9 @@ ActiveRecord::Schema.define(version: 20170620113809) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "booking_statuses"
+  add_foreign_key "bookings", "projects"
+  add_foreign_key "bookings", "users"
   add_foreign_key "equipment", "categories"
   add_foreign_key "equipment", "equipment_statuses"
 end
