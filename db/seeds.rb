218bbcc9bc61679deddf0a64f2d5d49baf6f3660
nvquynh <quynh.nguyen@haw-hamburg.de>
2017-06-23 10:30:08 +0200

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -48,63 +48,6 @@ end
 
 puts "10 projects created"
 
-6.times do |reserved_item|
-  Equipment.create!(
-      name: "Equipment #{reserved_item}",
-      description: "An equipment item that is still in the lab but already reserved for the choosen period of time. Only under special condition can the reservation be changed.",
-      equipment_status: rs, quantity: 1,
-      main_image: "http://via.placeholder.com/600x400",
-      thumb_image: "http://via.placeholder.com/300x200",
-      inv_nr: rand(10000...100000),
-      category_id: rand(1...7)
-
-  )
-end
-puts "6 reserved equipment items created"
-
-
-6.times do |borrowed_item|
-  Equipment.create!(
-      name: "Equipment #{borrowed_item}",
-      description: "An equipment item that is being borrowed and thus unavailable.",
-      equipment_status: br, quantity: 1,
-      main_image: "http://via.placeholder.com/600x400",
-      thumb_image: "http://via.placeholder.com/300x200",
-      inv_nr: rand(10000...100000),
-      category_id: rand(1...7)
-
-  )
-end
-puts "6 borrowed equipment items created"
-
-3.times do |dreported_item|
-  Equipment.create!(
-      name: "Equipment #{dreported_item}",
-      description: "An equipment item that is reported to be defective. Waiting for status confirmation from staff. Can be borrowed under consideration.",
-      equipment_status: bk, quantity: 1,
-      main_image: "http://via.placeholder.com/600x400",
-      thumb_image: "http://via.placeholder.com/300x200",
-      inv_nr: rand(10000...100000),
-      category_id: rand(1...7)
-
-  )
-end
-puts "3 defective equipment items created"
-
-
-3.times do |urepair_item|
-  Equipment.create!(
-      name: "Equipment #{urepair_item}",
-      description: "A defective equipment of my laboratory that is under repaired. Soon be available again.",
-      equipment_status: ur,
-      main_image: "http://via.placeholder.com/600x400",
-      thumb_image: "http://via.placeholder.com/300x200",
-      inv_nr: rand(10000...100000),
-      category_id: rand(1...7)
-  )
-end
-puts "3 under repair equipment items created"
-
 10.times do |available_item|
   Equipment.create!(
       name: "Equipment #{available_item}",
