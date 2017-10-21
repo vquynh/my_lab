6afd6e1d4eb81e2f0c8c65dd3fa95ea9d436ec04
Quynh <quynh.nguyen@deposit-solutions.com>
2017-10-21 23:19:10 +0200

    fixed double booking in menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require html.sortable
-//= require_tree .
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
 //= require jquery-ui
+//= require html.sortable
+//= require turbolinks
+//= require_tree .
+
 
