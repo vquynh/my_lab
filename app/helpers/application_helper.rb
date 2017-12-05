8a8fc8d3850f6002960e8d1972017e4a5bf462c4
Jonas <Jonas.Kern@haw-hamburg.de>
2017-12-05 14:15:46 +0100

    Navbar Design Changes
    Implemented collapsable navbar, changed login_helper to accept styles for rendering links as a list for better integration with navbar
    Know bugs: navbar does not collapse again after beeing opened (probably javascript related)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,16 @@
 module ApplicationHelper
-    def login_helper style = 'clear'
+    def login_helper(params = { list_class: "nav-item", link_class: "nav-link" } )
         if current_user.is_a?(GuestUser) 
-            (link_to "Register", new_user_registration_path, class: style) +
-            " ".html_safe +
-            (link_to "Login", new_user_session_path, class: style)
+            ('<li class="' + params[:list_class] + '">' + 
+            	(link_to "Register", new_user_registration_path, class: params[:link_class]) + ' ' +
+            '</li>' +
+            '<li class="' + params[:list_class] + '">' + 
+            	(link_to "Login", new_user_session_path, class: params[:link_class]) +
+            '</li>').html_safe
         else 
-            link_to "Logout", destroy_user_session_path, method: :delete, class: style 
+        	('<li class="' + params[:list_class] + '">' + 
+            	(link_to "Logout", destroy_user_session_path, method: :delete, class: params[:link_class]) +
+            '</li>').html_safe
         end 
     end
 end
