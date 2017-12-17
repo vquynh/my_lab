c1a1c95d37f5c16637986068f65b5ce0d54cfe68
Quynh <quynh.nguyen@deposit-solutions.com>
2017-12-17 16:19:11 +0100

    do not use aws to upload photo on local machine

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,10 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
-  storage :aws
-
+  if Rails.env.production?
+    storage :aws
+  else
+    storage :file
+  end
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
