e5872116c1bd0b319b097450d2ca6f6a1b3bbb89
Quynh <quynh.nguyen@deposit-solutions.com>
2018-01-10 10:56:42 +0100

    fixed recent booking items logic 2

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -3,9 +3,8 @@ class PageController < ApplicationController
     @new_items = Equipment.last(3)
     @new_projects = Project.last(3)
     @count = BookingItem.group(:equipment_id).count.first
-    last_booking_item_ids = BookingItem.distinct.last(3)
-    @recently_booked_items = Equipment.joins(:booking_items)
-                                 .where('id IN (?)', last_booking_item_ids)
+    last_booking_item_ids = BookingItem.distinct.last(3).select('equipment_id')
+    @recently_booked_items = Equipment.where('id IN (?)', last_booking_item_ids)
 
     fav_equipment_ids = BookingItem.select('equipment_id')
                             .group(:equipment_id)
